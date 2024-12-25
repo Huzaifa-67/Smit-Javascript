7193e9cf1263fe979e53b6ca0a7c1d8a1a1594e7
Huzaifa-67 <huzaifa.altaf04@gmail.com>
2024-12-25 10:09:32 +0500

    Initial commit

---
+++ 1.js
@@ -0,0 +1,7 @@
+let num = prompt("guess the number ")
+alert("give the number our friend as you assume")
+alert( "I give you number 50")
+alert("added both ")
+alert("give half charity ")
+alert(" and return our friend ")
+alert(" you have Reminder 25 ") 
